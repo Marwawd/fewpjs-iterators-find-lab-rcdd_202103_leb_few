5bc6ed32ab367446e22af1e0011171103a451274
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-28 18:09:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+ function superbowlWin(array){
+  let k=array.find( e => (e.result === "W"))
+  if(k=-1) return 'undefined'
+  else return e.year;
+ }
+
+ const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
+superbowlWin(record);
